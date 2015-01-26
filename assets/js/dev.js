3128e0da04378ed6186bc9e89d034e88cd7accd2
Shane Prendergast <contact@webknit.co.uk>
2015-01-26 17:23:57 +0000

    removing the code from the dev.js file

--- assets/js/dev.js
+++ assets/js/dev.js
@@ -1,42 +1,10 @@
 /* - BASE HTML TEMPLATE
 ------------------------------------------------- 
-	Description: JS Scripts
+	Description: Dev.js
 	Author:Shane Prendergast
 	Author URL:http://www.webknit.co.uk
 	Template URL:http://base.webknit.co.uk/
 	
-	If you're not using Grunt, or want to know how to use grunt, then have a read of the following post for more info.
-	http://shaneprendergast.co.uk/css/a-guide-to-base/
+	You can read more abotu this file - http://shaneprendergast.co.uk/web-design/dev-js/
 */
-
-// JS EXAMPLE
-
-var Base = Base || {};
-
-Base.functionName = function() {
-	
-	var variable = $('.variable');
-
-	function init() {
-
-		variable.click(functionOne);
-
-	}
-
-	function functionOne() {
-
-		// JS CODE
-
-	}
-
-	init();
-	
-};
-
-// ON DOC READY
-$(function() {
-	
-	new Base.functionName();
-	
-});
 
